0c113831075652d8be5e22708ed6044b7a55197f
Peppe2496 <giuseppemusellajob@gmail.com>
2023-12-07 14:26:04 +0100

    ended

--- src/org/lessons/java/wishlist/Wishlist.java
+++ src/org/lessons/java/wishlist/Wishlist.java
@@ -1,2 +1,44 @@
-package org.lessons.java.wishlist;public class Wishlist {
+package org.lessons.java.wishlist;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Wishlist {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Creo un insieme di regali senza duplicati
+        HashSet<String> wishlist = new HashSet<>();
+
+        boolean stop = false;
+        while (!stop){
+            System.out.println("Insert new gift(y/n)");
+            String answer = scanner.nextLine();
+            switch (answer){
+                case "y":
+                    //aggiungo un regalo
+                    System.out.println("Gift:");
+                    String gift = scanner.nextLine();
+                    boolean added = wishlist.add(gift);
+                    if (!added){
+                        System.out.println("Gift already added");
+                    }
+                    break;
+                case "n":
+                    stop = true;
+                    break;
+                default:
+                    System.out.println("Invalid answer");
+                    break;
+            }
+        }
+
+        System.out.println(wishlist);
+
+        for (String element: wishlist){
+            System.out.println(element.toLowerCase());
+        }
+
+        scanner.close();
+    }
 }
